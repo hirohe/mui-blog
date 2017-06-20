c09416ef24c9d2768c71bc20791cdca6d3d4e758
hirohe <hesreg@hotmail.com>
2017-06-20 17:05:05 +0800

    06/20 for test

--- src/components/CommentEditor/CommentEditor.js
+++ src/components/CommentEditor/CommentEditor.js
@@ -9,28 +9,41 @@ import styles from './CommentEditor.less';
 class CommentEditor extends React.Component {
 
   constructor(props) {
-    super(props)
+    super(props);
+
+    this.state = {
+      active: false
+    }
   }
 
   textFieldOnClick = () => {
-    console.log('clicked');
+    this.setState({active: true})
+  };
 
+  maskOnClick = () => {
+    this.setState({active: false})
   };
 
   render() {
     return (
-      <div className={styles.editor} style={{width: window.innerWidth - 20}}>
-        <TextField
-          className={styles.textField}
-          hintText="write your comment here"
-          multiLine={true}
-          rows={1}
-          rowsMax={4}
-          onClick={this.textFieldOnClick}
-        />
-        <FloatingActionButton mini className={styles.sendBtn}>
-          <SendIcon/>
-        </FloatingActionButton>
+      <div>
+        {this.state.active?<div className={styles.mask} onClick={this.maskOnClick}/>:null}
+        <div
+          className={this.state.active?styles.editorActive:styles.editor}
+          style={{width: this.state.active?(window.innerWidth - 20):null}}
+        >
+          <TextField
+            className={styles.textField}
+            hintText="write your comment here"
+            multiLine={true}
+            rows={1}
+            rowsMax={4}
+            onClick={this.textFieldOnClick}
+          />
+          <FloatingActionButton mini className={styles.sendBtn}>
+            <SendIcon/>
+          </FloatingActionButton>
+        </div>
       </div>
     )
   }
