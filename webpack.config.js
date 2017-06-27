c7aad6874909c4df111b37bc941a7b95c0973304
hirohe <hesreg@hotmail.com>
2017-06-27 18:48:45 +0800

    add icon font

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,22 @@
 export default (webpackConfig, env) => {
+  // global var for heightlight js
   webpackConfig.output.library = 'hljs';
+
+  webpackConfig.module.loaders.forEach(loader => {
+    //exclude url loader for fonts
+    if (loader.loader === 'url') {
+      loader.exclude.push(/\.(eot|ttf|woff|svg)$/)
+    }
+  });
+
+  //add file loader for fonts
+  webpackConfig.module.loaders.push({
+    test: /\.(eot|ttf|woff|svg)$/,
+    loader: 'file?name=assets/fonts/[name].[ext]',
+    exclude: /node_modules/
+  });
+
+  console.log(webpackConfig.module.loaders);
+
   return webpackConfig;
 }
