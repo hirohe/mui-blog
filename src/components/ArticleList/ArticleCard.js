16d1b26de57b0522a992ef6715feda53b0457bca
hirohe <hesreg@hotmail.com>
2017-07-03 11:39:21 +0800

    add labels to article card

--- src/components/ArticleList/ArticleCard.js
+++ src/components/ArticleList/ArticleCard.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { connect } from 'dva';
 import { Card, CardHeader, CardMedia, CardText, CardActions } from 'material-ui/Card';
+import Chip from 'material-ui/Chip';
 import IconButton from 'material-ui/IconButton';
 import FavoriteIcon from 'material-ui/svg-icons/action/favorite';
 import ShareIcon from 'material-ui/svg-icons/social/share';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import { red500, grey500 } from 'material-ui/styles/colors';
+
+import { randomMDColors } from '../../utils/common';
 
 import { likeArticle, dislikeArticle } from '../../services/Blog';
 
@@ -61,9 +64,15 @@ class ArticleCard extends React.Component {
     })
   };
 
+  onLabelSelect =(label) => {
+    console.log(label)
+  };
+
   render() {
 
-    const { id, title, sub_title, author, created_at, cover_url, preview, likes } = this.props.article;
+    const { id, title, sub_title, author, created_at, cover_url, preview, labels, likes } = this.props.article;
+    const labelList = labels.split(',');
+    const colors = randomMDColors(labelList.length);
 
     const createdDate = new Date(created_at);
 
@@ -83,7 +92,18 @@ class ArticleCard extends React.Component {
             <img src={cover_url} />
           </CardMedia>
         ):null}
-        <CardText onTouchTap={this.onSelect} className={styles.cardText}>{preview}</CardText>
+        <CardText onTouchTap={this.onSelect} className={styles.cardText}>
+          {preview}
+        </CardText>
+        {
+          labelList.length > 0?(
+            <div className={styles.labels}>
+              {
+                labelList.map((label, i) => <Chip style={{marginRight: 10}} backgroundColor={colors[i]} onTouchTap={()=>{this.onLabelSelect(label)}} key={i}>{label}</Chip>)
+              }
+            </div>
+          ):null
+        }
         <CardActions className={styles.cardActions}>
           <div className={styles.likes}>{this.state.like?likes+1:likes}</div>
           <IconButton onTouchTap={this.toggleLike}>
--- src/utils/common.js
+++ src/utils/common.js
@@ -1,9 +1,38 @@
+import {
+  red200, pink200, purple200, deepPurple200,
+  indigo200, blue200, lightBlue200, cyan200,
+  yellow200, orange200, green500
+} from 'material-ui/styles/colors';
+
 const delay = (timeout) => new Promise(resolve => setTimeout(resolve, timeout));
 
 const effectFactory = () => {
 
 };
 
+const randomMDColors = (n) => {
+  let colors = [
+    indigo200, blue200,
+    purple200, deepPurple200,
+    lightBlue200, cyan200,
+    red200, pink200,
+    yellow200, orange200, green500
+  ];
+
+  let _colors = [];
+  if (n <= colors.length) {
+    _colors = colors.sort((a, b) => {return .5 - Math.random()});
+    return _colors.splice(0, n);
+  } else {
+    for (let i = 0; i < (n/colors.length) >> 0; i++) {
+      _colors = _colors.concat(colors)
+    }
+    _colors = _colors.concat(colors.splice(0, n % colors.length));
+    return _colors.sort((a, b) => {return .5 - Math.random()});
+  }
+};
+
 export {
-  delay
+  delay,
+  randomMDColors
 }
